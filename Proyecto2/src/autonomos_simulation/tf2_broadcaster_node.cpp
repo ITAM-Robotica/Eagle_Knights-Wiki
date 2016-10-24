347dd11d3eb1d9f0f64470b890d1280221089058
Edgar Granados <edgar.granados@itam.mx>
2016-10-24 15:48:02 -0500

    fixed bug in simulated camera orientation

--- Proyecto2/src/autonomos_simulation/tf2_broadcaster_node.cpp
+++ Proyecto2/src/autonomos_simulation/tf2_broadcaster_node.cpp
@@ -8,6 +8,7 @@
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_ros/transform_broadcaster.h>
 #include <geometry_msgs/TransformStamped.h>
+#include "tf/tf.h"
 
 std::string robot_name;
 
@@ -18,6 +19,10 @@ void poseCallback(const gazebo_msgs::LinkStates& msg){
 	static tf2_ros::TransformBroadcaster br;
 	geometry_msgs::TransformStamped transformStamped;
 	
+	double yaw = tf::getYaw(msg.pose[1].orientation);
+	yaw = yaw - M_PI/2;
+	//aux_tf.transform.rotation = tf::createQuaternionFromRPY(0,0,-M_PI/2);
+
 	transformStamped.header.stamp = ros::Time::now();
 	transformStamped.header.frame_id = "world";
 	transformStamped.child_frame_id = robot_name;
@@ -26,7 +31,13 @@ void poseCallback(const gazebo_msgs::LinkStates& msg){
 	transformStamped.transform.translation.y = msg.pose[1].position.y;
 	transformStamped.transform.translation.z = msg.pose[1].position.z;
 	
-    transformStamped.transform.rotation = msg.pose[1].orientation;
+	// transformStamped.transform.rotation = tf::createQuaternionFromRPY(0, 0, yaw); //msg.pose[1].orientation;
+	tf::Quaternion q = tf::createQuaternionFromRPY(0, 0, yaw);
+	transformStamped.transform.rotation.x = q.x();
+	transformStamped.transform.rotation.y = q.y();
+	transformStamped.transform.rotation.z = q.z();
+	transformStamped.transform.rotation.w = q.w();
+    // transformStamped.transform.rotation = msg.pose[1].orientation;
 
     br.sendTransform(transformStamped);
 
